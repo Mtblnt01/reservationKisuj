b68fa77c3c39c0c14241c2f9f832ec494dc0a245
Mtblnt01 <balintmate963@gmail.com>
2025-11-06 08:13:38 +0100

    csacsa

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $response()->json();
+Route::get('/hello', function (Request $request) {
+    return $response()->json(['message'=> 'Hello, API']);
 })->middleware('auth:sanctum');
